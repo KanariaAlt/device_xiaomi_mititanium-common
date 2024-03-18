42b1f11916639c5c1617759915d80f80ea2c330a
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-18 14:06:34 +0800

    mititanium-common: Kang HotwordEnrollment from umi V14.0.5.0.TJBMIXM
    * SoundTrigger doesn't work with it for now for some reason, but at
      least this enables "Hey Google & Voice match" in Google Assistant
      app and makes "Hey Google" work when the screen is on
    
    Change-Id: Iaaba359e8480def2133cb3c24e5bfa716d755636

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -45,6 +45,7 @@ if [ -z "$ONLY_TARGET" ]; then
     write_headers "ysl vince sakura daisy mido tiffany rosy"
 
     # The standard common blobs
+    write_makefiles "${MY_DIR}/proprietary-files-misc.txt" true
     write_makefiles "${MY_DIR}/proprietary-files-qc-sys.txt" true
     write_makefiles "${MY_DIR}/proprietary-files-qc-vndr.txt" true
     write_makefiles "${MY_DIR}/proprietary-files-qc-vndr-32.txt" true
