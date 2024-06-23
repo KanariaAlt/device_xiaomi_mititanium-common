bc876cd771171a9e0c48d6362e935cb3069f3fcf
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:29:15 +0100

    mititanium-common: Move to new RFS install_symlink targets
    Change-Id: I9560fd0da00958d9349a0881b5a5f64a2c02e495

--- mititanium.mk
+++ mititanium.mk
@@ -10,6 +10,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Signed
 -include hardware/titanium_64/keys/product.mk
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Bootanimation
 TARGET_BOOTANIMATION_HALF_RES := true
 
