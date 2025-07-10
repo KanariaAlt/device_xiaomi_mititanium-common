b475996e9ca4517c735c5b964fd61d0eba3f8962
0xKANARIA <enashinonome264@gmail.com>
2025-07-10 00:19:18 +0000

    mititanium-common: change signed key
    Signed-off-by: 0xKANARIA <enashinonome264@gmail.com>

--- mititanium.mk
+++ mititanium.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Signed
--include vendor/extra/product.mk
+-include hardware/titanium_64/keys/product.mk
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
