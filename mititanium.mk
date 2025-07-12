632287c75e97123ae4fe66d0d16b2dad0d8389ac
0xKANARIA <enashinonome264@gmail.com>
2025-07-12 21:54:44 +0000

    mititanium-common: Build required libraries for new blobs
    Signed-off-by: 0xKANARIA <enashinonome264@gmail.com>

--- mititanium.mk
+++ mititanium.mk
@@ -120,7 +120,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
